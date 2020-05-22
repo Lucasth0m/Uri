4de642681ad0b9fded64d8d2277c160e4d928dee
Lucas Thomas <lucas.thomas99@hotmail.com>
2020-05-22 03:59:59 -0300

    Create 1156.cpp

---
+++ 1156.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    double i ,d=2;
+    double sum=1;
+
+    for (i = 3; i <= 39; i+=2){
+        sum+=(i/d);
+        d=d*2;
+    }
+    
+    cout << fixed << setprecision(2) << sum << endl;
+}
